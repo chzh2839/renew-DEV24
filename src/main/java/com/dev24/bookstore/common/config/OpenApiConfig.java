5a5beb2106de63cfa196d6dae659fbf778544770
borayang <chzh2839@gmail.com>
2026-07-15 13:06:30 +0900

    [#17] 스웨거 jwt인증 처리

--- src/main/java/com/dev24/bookstore/common/config/OpenApiConfig.java
+++ src/main/java/com/dev24/bookstore/common/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.dev24.bookstore.common.config;
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +9,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    // Security Scheme 정의
+    SecurityScheme securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+    // Security Requirement 정의
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
 
     @Bean
     public OpenAPI bookstoreOpenApi() {
@@ -14,6 +26,8 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("DEV24 Bookstore API")
                         .description("DEV24 도서 쇼핑몰 현대화 프로젝트 REST API 문서")
-                        .version("v1"));
+                        .version("v1"))
+                .addSecurityItem(securityRequirement)
+                .schemaRequirement("BearerAuth", securityScheme);
     }
 }
